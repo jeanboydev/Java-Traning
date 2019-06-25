2fc3c6dce8b1f44d51fea42f264e6070eabda3bb
jeanboy <jeanboy@foxmail.com>
2019-06-25 10:56:39 +0800

    update

--- src/main/java/com/jeanboy/traning/Test.java
+++ src/main/java/com/jeanboy/traning/Test.java
@@ -6,24 +6,24 @@ import java.util.concurrent.locks.ReentrantLock;
 
 public class Test {
 
-    public static void main(String[] args) {
-        System.out.println(Thread.currentThread().getName());
-
-        System.out.println(Thread.currentThread().getName() + "==getPriority==" + Thread.currentThread().getPriority() + "============线程开始创建=================");
-        MyThread myThread = new MyThread();
-        System.out.println(Thread.currentThread().getName() + "==getPriority==" + Thread.currentThread().getPriority() + "============线程已创建=================");
-        myThread.start();
-        System.out.println(Thread.currentThread().getName() + "==getPriority==" + Thread.currentThread().getPriority() + "============线程已开始=================");
-        try {
-            Thread.sleep(1000);
-            myThread.interrupt();
-            myThread.setDaemon(true);//将 myThread 设为 this 当前线程的守护线程
-//            myThread.stop();
-//            myThread.suspend();
-//            myThread.resume();
-            System.out.println(Thread.currentThread().getName() + "==getPriority==" + Thread.currentThread().getPriority() + "============线程执行完毕=================");
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
+//    public static void main(String[] args) {
+//        System.out.println(Thread.currentThread().getName());
+//
+//        System.out.println(Thread.currentThread().getName() + "==getPriority==" + Thread.currentThread().getPriority() + "============线程开始创建=================");
+//        MyThread myThread = new MyThread();
+//        System.out.println(Thread.currentThread().getName() + "==getPriority==" + Thread.currentThread().getPriority() + "============线程已创建=================");
+//        myThread.start();
+//        System.out.println(Thread.currentThread().getName() + "==getPriority==" + Thread.currentThread().getPriority() + "============线程已开始=================");
+//        try {
+//            Thread.sleep(1000);
+//            myThread.interrupt();
+//            myThread.setDaemon(true);//将 myThread 设为 this 当前线程的守护线程
+////            myThread.stop();
+////            myThread.suspend();
+////            myThread.resume();
+//            System.out.println(Thread.currentThread().getName() + "==getPriority==" + Thread.currentThread().getPriority() + "============线程执行完毕=================");
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//    }
 }
